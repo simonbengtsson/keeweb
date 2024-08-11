42cfa4dc1635a1c901ceb069b510af36890a0208
Simon Bengtsson <simonbengtsson@users.noreply.github.com>
2024-08-11 15:16:21 +0200

    Add deno config

---
+++ index.ts
@@ -0,0 +1,25 @@
+import { Application, Router } from 'https://deno.land/x/oak@v10.2.0/mod.ts';
+
+const app = new Application();
+
+app.use(async (ctx, next) => {
+    try {
+        await ctx.send({
+            root: `${Deno.cwd()}/dist`,
+            index: 'index.html'
+        });
+    } catch {
+        next();
+    }
+});
+
+const router = new Router();
+
+router.get('/api/time', (ctx) => {
+    ctx.response.body = { time: new Date().toISOString() };
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen({ port: 8000 });
