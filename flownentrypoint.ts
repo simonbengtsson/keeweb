bc530046410d7f4d6e8bf9ca1eff12ea594552d7
Simon Bengtsson <simonbengtsson@users.noreply.github.com>
2024-08-12 16:59:06 +0200

    Rename files

--- flownentrypoint.ts
+++ flownentrypoint.ts
@@ -1,12 +1,19 @@
+// ONLY FOR DEV
+
 import { Hono } from 'jsr:@hono/hono';
 import { serveStatic } from 'jsr:@hono/hono/deno';
-import { __app } from './flownservices.ts';
+import { __app, initialize } from './flownservices.ts';
 
-import './index.ts'; // App entry point
+import './flown.ts'; // App entry point
 
 __app().then((config) => {
+    // Should parse cookie and/or url here
+    initialize({
+        user: { id: 'abc', name: 'John Doe' },
+        instance: { id: 'abc' },
+    });
     const app = new Hono();
-    app.use(async (c, next) => {
+    app.use(async (_, next) => {
         console.log('Do auth and services setup');
         await next();
     });
--- flownservices.ts
+++ flownservices.ts
@@ -1,17 +1,25 @@
+// ONLY FOR DEV. WILL BE REPLACED WITH npm/jsr module later
+
 let configResolver: any;
 
-type AppConfig = {
-    name: string;
-    description?: string;
-    icon?: string;
-    backend?: (req: any) => any;
+let state: {
+    user: any,
+    instance: any,
+};
+
+export type AppConfig = {
+    // name: string;
+    // description?: string;
+    // icon?: string;
+    // backend?: (req: any) => any;
+    // runNpmBuild: boolean,
     root?: string;
     permissions?: {
         database?: boolean;
     };
 };
 
-type SetupFunction = () => AppConfig | Promise<AppConfig>;
+type SetupFunction = () => AppConfig;
 
 export async function setupApp(func: SetupFunction) {
     configResolver = Promise.withResolvers();
@@ -20,19 +28,24 @@ export async function setupApp(func: SetupFunction) {
 }
 
 export function getInstance() {
-    return {
-        id: 'abc'
-    };
+    if (!state) {
+        throw new Error('App not initialized');
+    }
+    return state.instance;
 }
 
 export async function getDatabase() {
+    if (!state) {
+        throw new Error('App not initialized');
+    }
     return await Deno.openKv();
 }
 
 export function getUser(): { id: string } {
-    return {
-        id: 'abc'
-    };
+    if (!state) {
+        throw new Error('App not initialized');
+    }
+    return state.user;
 }
 
 export async function __app() {
@@ -40,4 +53,8 @@ export async function __app() {
         throw new Error('App not initialized');
     }
     return await configResolver.promise;
+}
+
+export function initialize(envState: any) {
+    state = envState;
 }
