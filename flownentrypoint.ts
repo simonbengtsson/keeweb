cd971300348c99a64779feb7b9364138426e3ab7
Simon Bengtsson <simonbengtsson@users.noreply.github.com>
2024-08-14 23:06:39 +0200

    Fix formatting

--- flownentrypoint.ts
+++ flownentrypoint.ts
@@ -1,16 +1,21 @@
-import { Hono } from "jsr:@hono/hono";
-import { serveStatic } from "jsr:@hono/hono/deno";
-import { __app } from "./flownservices.ts";
+import { Hono } from 'jsr:@hono/hono';
+import { serveStatic } from 'jsr:@hono/hono/deno';
+import { __app } from './flownservices.ts';
 
-import "./flown.ts";
+import './flown.ts';
 
 __app().then((config) => {
-  const app = new Hono();
-  app.use(async (_, next) => {
-    console.log("Do auth and services setup");
-    await next();
-  });
-  app.use("*", serveStatic({ root: "./dist" }));
-  app.mount("/", config.backend);
-  Deno.serve(app.fetch);
+    const app = new Hono();
+    app.use(async (ctx, next) => {
+        const appId = ctx.req.header('x-flown-app-id');
+        if (!appId) {
+            console.log('Disallowed non proxy request');
+            return Response.redirect('https://flown.io');
+        }
+        await next();
+    });
+    app.use('*', serveStatic({ root: './dist' }));
+    app.mount('/', config.backend);
+
+    Deno.serve(app.fetch);
 });
